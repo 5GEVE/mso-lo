0adc1d00cf7bd85de300100f8a548af0962e576c
Francesco Lombardo <franclombardo@gmail.com>
2019-07-25 13:16:36 +0200

    wip: osm driver with mock data

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# adaptation-layer
+# 5G-Eve - Adaptation Layer
+
+## Development
+
+
+
+## Installation
+
+### Docker installation
+
+1) clone the project
+2) Build the image drom the project's root directory:
+    
+        $ docker build -t adaptation-layer .
+
+#### To launch:
+
+    $ docker run -p 5000:5000 --name adl0 adaptation-layer
+
+
+## Credits
+
+## Contact Info
+
+## License
--- adaptation_layer/driver/manager.py
+++ adaptation_layer/driver/manager.py
@@ -2,6 +2,19 @@ from .interface import Driver
 from .osm import OSM
 from .onap import ONAP
 
+# mock nvfo list
+nfvo_list = [
+    {
+        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        "name": "OSM-Turin",
+        "nfvoType": "OSM",
+        "location": "Italy",
+        "uri": "https://osm-turin.5g-eve.eu",
+        "createdAt": "2019-06-29T09:12:33.001Z",
+        "updatedAt": "2019-06-29T09:12:33.001Z"
+    }
+]
+
 
 def get_driver(nfvo_id) -> Driver:
     # checks the nfvo_id against the database and gets the type
@@ -13,3 +26,13 @@ def get_driver(nfvo_id) -> Driver:
         return ONAP()
     else:
         raise NotImplementedError('Driver type: {} is not implemented'.format(type))
+
+
+def get_nfvo_list() -> list:
+    # return a mock list
+    return nfvo_list
+
+
+def get_nfvo(nfvo_id) -> dict:
+    # return a mock nvfo data
+    return nfvo_list[0]
