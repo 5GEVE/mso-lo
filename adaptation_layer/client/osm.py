1a8add8e5d4d1e99e203fdcf5060730cfc1f56c3
Francesco Lombardo <franclombardo@gmail.com>
2019-10-03 13:23:39 +0200

    add support json/yaml response

--- adaptation_layer/client/osm.py
+++ adaptation_layer/client/osm.py
@@ -1,4 +1,6 @@
 import requests
+import json as JSON
+import yaml as YAML
 import os
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
@@ -23,24 +25,35 @@ class Client(object):
         self._project = project
         self._base_path = 'https://{0}:{1}/osm'.format(self._host, so_port)
         self._headers = {"Content-Type": "application/yaml", "accept": "application/json"}
-        # token = self.authenticate()
-        # if token['error']:
-        #    raise Exception(token['error'])
-        # self._headers['Authorization'] = 'Bearer {}'.format(token['data'])
+        token = self.authenticate()
+        if 'error' in token:
+            raise Exception(token['error'])
+        self._headers['Authorization'] = 'Bearer {}'.format(token['data']['id'])
 
     def _exec_get(self, url=None, params=None, headers=None):
-
+        print(headers)
         result = {}
         try:
             r = requests.get(url, params=params, verify=False, stream=True, headers=headers)
         except Exception as e:
             result['error'] = str(e)
             return result
+        
         if r.status_code in (200, 201, 202, 204):
-            result['data'] = r.json
+            if 'application/json' == r.headers['content-type']:
+                result['data'] = r.json()
+            elif 'application/yaml' == r.headers['content-type']:
+                result['data'] = JSON.load(JSON.dumps(YAML.load(r.text), sort_keys=True, indent=2))
+            else:
+                result['data'] = r.text
         else:
-            result['error'] = r.json
-        print(result)
+            if 'application/json' == r.headers['content-type']:
+                result['error'] = r.json()
+            elif 'application/yaml' == r.headers['content-type']:
+                result['error'] = JSON.loads(JSON.dumps(YAML.load(r.text), sort_keys=True, indent=2))
+            else:
+                result['error'] = r.text
+
         return result
 
     def _exec_post(self, url=None, data=None, json=None, headers=None):
@@ -52,10 +65,15 @@ class Client(object):
             result['error'] = str(e)
             return result
         if r.status_code in (200, 201, 202, 204):
-            result['data'] = r.json
+            if 'application/json' == r.headers['content-type']:
+                result['data'] = r.json()
+            elif 'application/yaml' == r.headers['content-type']:
+                result['data'] = JSON.loads(JSON.dumps(YAML.load(r.text), sort_keys=True, indent=2))
+            else:
+                result['data'] = r.text
         else:
-            result['error'] = r.json
-        print(result)
+            result['error'] = r.text
+
         return result
 
     def authenticate(self):
@@ -65,36 +83,36 @@ class Client(object):
 
     def ns_list(self):
         _url = "{0}/nslcm/v1/ns_instances_content".format(self._base_path)
-        return self._exec_get(_url)
+        return self._exec_get(_url, headers=self._headers)
 
     def vnf_list(self):
         _url = "{0}/nslcm/v1/vnfrs".format(self._base_path)
-        return self._exec_get(_url)
+        return self._exec_get(_url, headers=self._headers)
 
     def ns_create(self, ns_data):
         _url = "{0}/nslcm/v1/ns_instances_content".format(self._base_path)
-        return self._exec_post(_url, json=ns_data)
+        return self._exec_post(_url, json=ns_data, headers=self._headers)
 
     def ns_op_list(self, id):
         _url = "{0}/nslcm/v1/ns_lcm_op_occs/?nsInstanceId={1}".format(
             self._base_path, id)
-        return self._exec_get(_url)
+        return self._exec_get(_url, headers=self._headers)
 
     def ns_op(self, id):
         _url = "{0}/nslcm/v1/ns_lcm_op_occs/{1}".format(self._base_path, id)
-        return self._exec_get(_url)
+        return self._exec_get(_url, headers=self._headers)
 
-    def ns_action(self, id, action_payload):
-        _url = "{0}/nslcm/v1/ns_instances/{1}/action".format(self._base_path, id)
-        return self._exec_post(_url, json=action_payload)
+    def ns_action(self, ns_id, action_payload):
+        _url = "{0}/nslcm/v1/ns_instances/{1}/action".format(self._base_path, ns_id)
+        return self._exec_post(_url, json=action_payload, headers=self._headers)
 
-    def ns_delete(self, id, force=None):
+    def ns_delete(self, ns_id, force=None):
         result = {}
         query_path = ''
         if force:
             query_path = '?FORCE=true'
         _url = "{0}/nslcm/v1/ns_instances_content/{1}{2}".format(
-            self._base_path, id, query_path)
+            self._base_path, ns_id, query_path)
         try:
             r = requests.delete(_url, params=None, verify=False, headers=self._headers)
         except Exception as e:
@@ -109,8 +127,8 @@ class Client(object):
     def ns_get(self, id):
         _url = "{0}/nslcm/v1/ns_instances_content/{1}".format(
             self._base_path, id)
-        return self._exec_get(_url)
+        return self._exec_get(_url, headers=self._headers)
 
     def vnf_get(self, id):
         _url = "{0}/nslcm/v1/vnfrs/{1}".format(self._base_path, id)
-        return self._exec_get(_url)
+        return self._exec_get(_url, headers=self._headers)
