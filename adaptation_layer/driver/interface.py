54d8677368047e0e9113e39823ae0d93cd009d23
Matteo Pergolesi <matpergo@gmail.com>
2019-10-16 10:28:30 +0200

    Fix list typing.

--- adaptation_layer/driver/interface.py
+++ adaptation_layer/driver/interface.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 
 class Driver(ABC):
-
     """
     methods related to Virtual Network Function
     """
+
     @abstractmethod
     def get_vnf_list(args=None) -> list:
         raise NotImplementedError("The method is not implemented")
@@ -17,26 +18,27 @@ class Driver(ABC):
     """
     methods related to Network Service Descriptor
     """
+
     @abstractmethod
-    def get_ns_list(args=None) -> list:
+    def get_ns_list(args=None) -> List[dict]:
         raise NotImplementedError("The method is not implemented")
 
     @abstractmethod
-    def create_ns(args=None) -> list:
+    def create_ns(args=None) -> List[dict]:
         raise NotImplementedError("The method is not implemented")
 
     @abstractmethod
-    def get_ns(nsId: str, args=None) -> list:
+    def get_ns(nsId: str, args=None) -> List[dict]:
         raise NotImplementedError("The method is not implemented")
 
     @abstractmethod
-    def instantiate_ns(nsId: str, args=None) -> list:
+    def instantiate_ns(nsId: str, args=None) -> List[dict]:
         raise NotImplementedError("The method is not implemented")
 
     @abstractmethod
-    def terminate_ns(nsId: str, args=None) -> list:
+    def terminate_ns(nsId: str, args=None) -> List[dict]:
         raise NotImplementedError("The method is not implemented")
 
     @abstractmethod
-    def scale_ns(nsId: str, args=None) -> list:
+    def scale_ns(nsId: str, args=None) -> List[dict]:
         raise NotImplementedError("The method is not implemented")
