76989483187ae483825d7de69cba7a9d2f2636b1
Matteo Pergolesi <matpergo@gmail.com>
2019-10-22 17:36:19 +0200

    Remove methods related to VNF instance management.

--- adaptation_layer/driver/interface.py
+++ adaptation_layer/driver/interface.py
@@ -4,18 +4,6 @@ from typing import List, Dict
 
 class Driver(ABC):
     """
-    methods related to Virtual Network Function
-    """
-
-    @abstractmethod
-    def get_vnf_list(args=None) -> list:
-        raise NotImplementedError("The method is not implemented")
-
-    @abstractmethod
-    def get_vnf(vnfId: str, args=None) -> list:
-        raise NotImplementedError("The method is not implemented")
-
-    """
     methods related to Network Service Descriptor
     """
 
