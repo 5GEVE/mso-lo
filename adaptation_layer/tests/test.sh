a6b966ce9c7b3a9892e0906ff0ba19fe814239b4
Matteo Pergolesi <matpergo@gmail.com>
2019-10-22 15:20:05 +0200

    Rename osm openapi yaml file.

--- adaptation_layer/tests/test.sh
+++ adaptation_layer/tests/test.sh
@@ -8,7 +8,7 @@ nodeenv -p &
 deactivate &&
 source .venv/bin/activate &&
 npm install -g @stoplight/prism-cli &&
-prism mock tests/osm_fixed.yaml --port 9999 &
+prism mock tests/osm-openapi.yaml --port 9999 &
 
 # run pytest
 
