f54e776a26718c1aa0b71bb626d506ab306194eb
Francesco Lombardo <franclombardo@gmail.com>
2019-10-23 15:26:20 +0200

    osm im translator

--- adaptation_layer/driver/onap.py
+++ adaptation_layer/driver/onap.py
@@ -3,10 +3,8 @@ from .interface import Driver
 
 class ONAP(Driver):
 
-    @classmethod
     def get_nsd_list(self, args) -> dict:
         return {'NSDs': ['onap_nsd_1', 'onap_nsd_2']}
 
-    @classmethod
     def get_nsd(self, nsd_id: str, args) -> dict:
         return {'NSD': {'id': 'onap_nsd_1'}}
