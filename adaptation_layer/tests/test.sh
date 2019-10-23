340fb8d70e06300d98205fc1f1e31ea132c40f72
Matteo Pergolesi <matpergo@gmail.com>
2019-10-23 19:29:24 +0200

    Fix errors due to pipenv run and parallel executions (progress bars).

--- adaptation_layer/tests/test.sh
+++ adaptation_layer/tests/test.sh
@@ -4,14 +4,18 @@
 
 export PIPENV_VENV_IN_PROJECT=1
 export TESTING=True
-pipenv install --dev &&
-pipenv run nodeenv --debug --prebuilt --python-virtualenv --jobs=8 &&
-pipenv run npm install -g @stoplight/prism-cli &&
-pipenv run prism mock tests/osm-openapi.yaml --port 9999 &>/dev/null;
+pipenv  install --dev --skip-lock;
+source .venv/bin/activate
+nodeenv --python-virtualenv --jobs=8;
+deactivate
+source .venv/bin/activate
+npm install --no-progress -g @stoplight/prism-cli;
+prism mock tests/osm-openapi.yaml --port 9999 &>/dev/null &
 PRISM_PID=$!
-printf "$PRISM_PID"
-sleep 1
-printf "run python here"
-#pipenv run cd tests && python test_osm.py
+printf "$PRISM_PID\n"
+sleep 2
+cd tests &&
+python test_osm.py
 kill ${PRISM_PID}
 exit 0
+
