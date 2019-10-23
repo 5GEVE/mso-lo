576cecbd34101f0f709ddc8708309cc39ae92a74
Matteo Pergolesi <matpergo@gmail.com>
2019-10-23 18:07:51 +0200

    Improve test script. To test if we can kill prism like that.

--- adaptation_layer/tests/test.sh
+++ adaptation_layer/tests/test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# to be run in adaptation-layer module dir
+
 export PIPENV_VENV_IN_PROJECT=1
 export TESTING=True
 pipenv install --dev &&
-source .venv/bin/activate &&
-nodeenv -p &
-deactivate &&
-source .venv/bin/activate &&
-npm install -g @stoplight/prism-cli &&
-prism mock tests/osm-openapi.yaml --port 9999 &
-
-# run pytest
-
+pipenv run nodeenv --debug --prebuilt --python-virtualenv --jobs=8 &&
+pipenv run npm install -g @stoplight/prism-cli &&
+pipenv run prism mock tests/osm-openapi.yaml --port 9999 &>/dev/null;
+PRISM_PID=$!
+printf "$PRISM_PID"
+sleep 1
+printf "run python here"
+#pipenv run python test_osm.py
+kill ${PRISM_PID}
+exit 0
