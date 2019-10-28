4f2d9b533ff381fabd6948e2723e4eb49590e4d4
Matteo Pergolesi <matpergo@gmail.com>
2019-10-28 14:58:15 +0100

    Prepare stub methods for ONAP driver.

--- adaptation_layer/driver/onap.py
+++ adaptation_layer/driver/onap.py
@@ -1,10 +1,24 @@
+from typing import Dict, List
+
 from .interface import Driver
 
 
 class ONAP(Driver):
 
-    def get_nsd_list(self, args) -> dict:
-        return {'NSDs': ['onap_nsd_1', 'onap_nsd_2']}
+    def get_ns_list(self, args: Dict = None) -> List[Dict]:
+        pass
 
-    def get_nsd(self, nsd_id: str, args) -> dict:
-        return {'NSD': {'id': 'onap_nsd_1'}}
+    def create_ns(self, args: Dict = None) -> List[Dict]:
+        pass
+
+    def get_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+        pass
+
+    def instantiate_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+        pass
+
+    def terminate_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+        pass
+
+    def scale_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+        pass
