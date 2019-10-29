57ca13c22bdb27c402e626b0efa317079b6511fb
Matteo Pergolesi <matpergo@gmail.com>
2019-10-29 15:54:16 +0100

    Fix type hints on interface and drivers.

--- adaptation_layer/driver/onap.py
+++ adaptation_layer/driver/onap.py
@@ -8,17 +8,17 @@ class ONAP(Driver):
     def get_ns_list(self, args: Dict = None) -> List[Dict]:
         pass
 
-    def create_ns(self, args: Dict = None) -> List[Dict]:
+    def create_ns(self, args: Dict = None) -> Dict:
         pass
 
-    def get_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+    def get_ns(self, nsId: str, args: Dict = None) -> Dict:
         pass
 
-    def instantiate_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+    def instantiate_ns(self, nsId: str, args: Dict = None) -> Dict:
         pass
 
-    def terminate_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+    def terminate_ns(self, nsId: str, args: Dict = None) -> Dict:
         pass
 
-    def scale_ns(self, nsId: str, args: Dict = None) -> List[Dict]:
+    def scale_ns(self, nsId: str, args: Dict = None) -> Dict:
         pass
