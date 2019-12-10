168a8e7dcd382f7efe3cfce126bdf7e1216f6680
Francesco Lombardo <franclombardo@gmail.com>
2019-12-10 18:16:30 +0100

    config.py code clean

--- adaptation_layer/config.py
+++ adaptation_layer/config.py
@@ -1,17 +1,14 @@
 import os
-
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 from error_handler import init_errorhandler
-
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data/mso-lo.db')
+        'sqlite:///' + os.path.join(basedir, 'data/mso-lo.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
