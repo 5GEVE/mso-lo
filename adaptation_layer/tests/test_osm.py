0d16150bc5f26741b83452babb5706603e54bd19
Matteo Pergolesi <matpergo@gmail.com>
2020-01-11 16:41:29 +0100

    Complete fix for request_mock and response_schemas imports.

--- adaptation_layer/tests/test_osm.py
+++ adaptation_layer/tests/test_osm.py
@@ -1,12 +1,13 @@
 #!flask/bin/python
 import unittest
 
-import request_mock
-import response_schemas
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError, SchemaError
 
 from app import app
+from .request_mock import mock_ns, mock_ns_instantiate, mock_ns_scale, mock_ns_terminate
+from .response_schemas import ns_create_schema, ns_lcm_op_occ_schema, \
+    ns_list_schema, ns_schema
 
 
 class OSMTestCase(unittest.TestCase):
@@ -23,7 +24,7 @@ class OSMTestCase(unittest.TestCase):
     def test_get_ns_list_200(self):
         res = self.client().get('/nfvo/1/ns_instances?__code=200')
         try:
-            validate(res.json, response_schemas.ns_list_schema)
+            validate(res.json, ns_list_schema)
         except (ValidationError, SchemaError) as e:
             self.fail(msg=e.message)
         self.assertEqual(res.status_code, 200)
@@ -37,7 +38,7 @@ class OSMTestCase(unittest.TestCase):
         res = self.client().get(
             '/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=200')
         try:
-            validate(res.json, response_schemas.ns_schema)
+            validate(res.json, ns_schema)
         except (ValidationError, SchemaError) as e:
             self.fail(msg=e.message)
         self.assertEqual(res.status_code, 200)
@@ -55,55 +56,55 @@ class OSMTestCase(unittest.TestCase):
     # Check status codes 201, 401, 404, headers and payload for create_ns()
     def test_create_ns_201(self):
         res = self.client().post('/nfvo/1/ns_instances?__code=201',
-                                 json=request_mock.mock_ns)
+                                 json=mock_ns)
         try:
-            validate(res.json, response_schemas.ns_create_schema)
+            validate(res.json, ns_create_schema)
         except (ValidationError, SchemaError) as e:
             self.fail(msg=e.message)
         self.assertEqual(res.status_code, 201)
 
     def test_create_ns_400(self):
         res = self.client().post('/nfvo/1/ns_instances?__code=400',
-                                 json=request_mock.mock_ns)
+                                 json=mock_ns)
         self.assertEqual(res.status_code, 400)
 
     def test_create_ns_401(self):
         res = self.client().post('/nfvo/1/ns_instances?__code=401',
-                                 json=request_mock.mock_ns)
+                                 json=mock_ns)
         self.assertEqual(res.status_code, 401)
 
     # Check status codes 202, 401, 404, headers and payload for instantiate_ns()
     def test_instantiate_ns_202(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=202',
-                                 json=request_mock.mock_ns_instantiate)
+                                 json=mock_ns_instantiate)
         # TODO check header
         self.assertEqual(res.status_code, 202)
 
     def test_instantiate_ns_400(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=400',
-                                 json=request_mock.mock_ns_instantiate)
+                                 json=mock_ns_instantiate)
         self.assertEqual(res.status_code, 400)
 
     def test_instantiate_ns_401(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=401',
-                                 json=request_mock.mock_ns_instantiate)
+                                 json=mock_ns_instantiate)
         self.assertEqual(res.status_code, 401)
 
     # Check status codes 202, 401, 404, headers and payload for terminate_ns()
     def test_terminate_ns_202(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=202',
-                                 json=request_mock.mock_ns_terminate)
+                                 json=mock_ns_terminate)
         # TODO check header
         self.assertEqual(res.status_code, 202)
 
     def test_terminate_ns_404(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=404',
-                                 json=request_mock.mock_ns_terminate)
+                                 json=mock_ns_terminate)
         self.assertEqual(res.status_code, 404)
 
     def test_terminate_ns_401(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=401',
-                                 json=request_mock.mock_ns_terminate)
+                                 json=mock_ns_terminate)
         self.assertEqual(res.status_code, 401)
 
     # Check status codes 202, 401, 404, headers and payload for delete_ns()
@@ -125,18 +126,18 @@ class OSMTestCase(unittest.TestCase):
     # Check status codes 202, 401, 404, headers and payload for scale_ns()
     def test_scale_ns_202(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/scale?__code=202',
-                                 json=request_mock.mock_ns_scale)
+                                 json=mock_ns_scale)
         # TODO check header
         self.assertEqual(res.status_code, 202)
 
     def test_scale_ns_404(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/scale?__code=404',
-                                 json=request_mock.mock_ns_scale)
+                                 json=mock_ns_scale)
         self.assertEqual(res.status_code, 404)
 
     def test_scale_ns_401(self):
         res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/scale?__code=401',
-                                 json=request_mock.mock_ns_scale)
+                                 json=mock_ns_scale)
         self.assertEqual(res.status_code, 401)
 
     # Check status codes 200, 401, 404, headers and payload for get_ns_lcm_op_occs_()
@@ -144,7 +145,7 @@ class OSMTestCase(unittest.TestCase):
         res = self.client().get(
             '/nfvo/1/ns_lcm_op_occs/49ccb6a2-5bcd-4f35-a2cf-7728c54c48b7?__code=200')
         try:
-            validate(res.json, response_schemas.ns_lcm_op_occ_schema)
+            validate(res.json, ns_lcm_op_occ_schema)
         except (ValidationError, SchemaError) as e:
             self.fail(msg=e.message)
         self.assertEqual(res.status_code, 200)
