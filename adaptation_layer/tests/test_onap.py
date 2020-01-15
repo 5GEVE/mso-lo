64362c9bad9fe1c2a00c000381be7eed26681715
Matteo Pergolesi <matpergo@gmail.com>
2020-01-15 16:12:14 +0100

    Add mocked ONAP orchestrator in DB. Change nfvo id in tests.

--- adaptation_layer/tests/test_onap.py
+++ adaptation_layer/tests/test_onap.py
@@ -24,7 +24,7 @@ class OnapTestCase(unittest.TestCase):
 
     # Check status codes 200, 404, headers and payload for get_ns_list()
     def test_get_ns_list_200(self):
-        res = self.client().get('/nfvo/1/ns_instances?__code=200')
+        res = self.client().get('/nfvo/2/ns_instances?__code=200')
         try:
             validate(res.json, ns_list_schema)
         except (ValidationError, SchemaError) as e:
@@ -34,7 +34,7 @@ class OnapTestCase(unittest.TestCase):
     # Check status codes 200, 404, headers and payload for get_ns()
     def test_get_ns_200(self):
         res = self.client().get(
-            '/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=200')
+            '/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=200')
         try:
             validate(res.json, ns_schema)
         except (ValidationError, SchemaError) as e:
@@ -43,12 +43,12 @@ class OnapTestCase(unittest.TestCase):
 
     def test_get_ns_404(self):
         res = self.client().get(
-            '/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=404')
+            '/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=404')
         self.assertEqual(res.status_code, 404)
 
     # Check status codes 201, 404, headers and payload for create_ns()
     def test_create_ns_201(self):
-        res = self.client().post('/nfvo/1/ns_instances?__code=201', json=mock_ns)
+        res = self.client().post('/nfvo/2/ns_instances?__code=201', json=mock_ns)
         try:
             validate(res.json, ns_schema)
         except (ValidationError, SchemaError) as e:
@@ -56,48 +56,48 @@ class OnapTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
 
     def test_create_ns_400(self):
-        res = self.client().post('/nfvo/1/ns_instances?__code=400', json=mock_ns)
+        res = self.client().post('/nfvo/2/ns_instances?__code=400', json=mock_ns)
         self.assertEqual(res.status_code, 400)
 
     # Check status codes 202, 400, 404, headers and payload for instantiate_ns()
     def test_instantiate_ns_202(self):
-        res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=202')
+        res = self.client().post('/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=202')
         # TODO add header check method
         self.assertEqual(res.status_code, 202)
 
     def test_instantiate_ns_400(self):
-        res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=400')
+        res = self.client().post('/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=400')
         self.assertEqual(res.status_code, 400)
 
     def test_instantiate_ns_404(self):
-        res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=404')
+        res = self.client().post('/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/instantiate?__code=404')
         self.assertEqual(res.status_code, 404)
 
     # Check status codes 202, 404, headers and payload for terminate_ns()
     def test_terminate_ns_202(self):
-        res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=202',
+        res = self.client().post('/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=202',
                                  json=mock_ns_terminate)
         self.assertEqual(res.status_code, 202)
 
     def test_terminate_ns_404(self):
-        res = self.client().post('/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=404',
+        res = self.client().post('/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=404',
                                  json=mock_ns_terminate)
         self.assertEqual(res.status_code, 404)
 
     # Check status codes 202, 404, headers and payload for delete_ns()
     def test_delete_ns_202(self):
         res = self.client().delete(
-            '/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=202')
+            '/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=202')
         self.assertEqual(res.status_code, 202)
 
     def test_delete_ns_404(self):
         res = self.client().delete(
-            '/nfvo/1/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=404')
+            '/nfvo/2/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7?__code=404')
         self.assertEqual(res.status_code, 404)
 
     # Check status codes 200, 404, headers and payload for get_ns_lcm_op_occs_()
     def test_get_ns_lcm_op_occs_200(self):
-        res = self.client().get('/nfvo/1/ns_lcm_op_occs/49ccb6a2-5bcd-4f35-a2cf-7728c54c48b7?__code=200')
+        res = self.client().get('/nfvo/2/ns_lcm_op_occs/49ccb6a2-5bcd-4f35-a2cf-7728c54c48b7?__code=200')
         try:
             validate(res.json, ns_lcm_op_occ_schema)
         except (ValidationError, SchemaError) as e:
@@ -105,12 +105,12 @@ class OnapTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_get_ns_lcm_op_occs_404(self):
-        res = self.client().get('/nfvo/1/ns_lcm_op_occs/49ccb6a2-5bcd-4f35-a2cf-7728c54c48b7?__code=404')
+        res = self.client().get('/nfvo/2/ns_lcm_op_occs/49ccb6a2-5bcd-4f35-a2cf-7728c54c48b7?__code=404')
         self.assertEqual(res.status_code, 404)
 
     # Check status codes 200, headers and payload for get_ns_lcm_op_occs_list()
     def test_get_ns_lcm_op_occs_list_200(self):
-        res = self.client().get('/nfvo/1/ns_lcm_op_occs?__code=200')
+        res = self.client().get('/nfvo/2/ns_lcm_op_occs?__code=200')
         try:
             validate(res.json, ns_lcm_op_occ_list_schema)
         except (ValidationError, SchemaError) as e:
