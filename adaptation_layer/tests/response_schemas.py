ae7ac4050b5eecd5921c44e382ddcdb5d19674df
Matteo Pergolesi <matpergo@gmail.com>
2020-04-30 14:16:28 +0200

    Get openapi path from environment variable. Fix docker-compose files accordingly.

--- adaptation_layer/tests/response_schemas.py
+++ adaptation_layer/tests/response_schemas.py
@@ -11,37 +11,19 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import fnmatch
 import os
 
 from prance import ResolvingParser
 
+OPENAPI_PATH = os.environ.get("OPENAPI_PATH",
+                              '../../openapi/MSO-LO-swagger-resolved.yaml')
 
-def _get_file_path(directory: str) -> str:
-    try:
-        for file in os.listdir(directory):
-            if fnmatch.fnmatch(file, 'MSO-LO-?.?-swagger-resolved.yaml'):
-                return os.path.join(directory, file)
-    except FileNotFoundError:
-        pass
-
-
-# Test in container
-file_path = _get_file_path('./openapi/')
-if file_path is None:
-    # Test in dev environment
-    file_path = _get_file_path('../../openapi/')
-if file_path is None:
-    file_path = _get_file_path('../openapi/')
-if file_path is None:
-    raise FileNotFoundError("Openapi directory not found.")
-
-openapi = ResolvingParser(file_path).specification
+openapi = ResolvingParser(OPENAPI_PATH).specification
 
 nfvo_schema = openapi["definitions"]["NFVO"]
 nfvo_list_schema = {
-    "type": "array",
-    "items": nfvo_schema
+  "type": "array",
+  "items": nfvo_schema
 }
 
 id_schema = openapi["definitions"]["Identifier"]
@@ -49,13 +31,13 @@ id_schema = openapi["definitions"]["Identifier"]
 ns_schema = openapi["definitions"]["NsInstance"]
 
 ns_list_schema = {
-    "type": "array",
-    "items": ns_schema
+  "type": "array",
+  "items": ns_schema
 }
 
 ns_lcm_op_occ_schema = openapi["definitions"]["NsLcmOpOcc"]
 
 ns_lcm_op_occ_list_schema = {
-    "type": "array",
-    "items": ns_lcm_op_occ_schema
+  "type": "array",
+  "items": ns_lcm_op_occ_schema
 }
