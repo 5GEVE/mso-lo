1bc37884c162c478e8d4dd860ef6ed2cae242fae
Matteo Pergolesi <matpergo@gmail.com>
2020-05-19 14:25:57 +0200

    Convert nfvo json correctly. Better exception handling.

--- adaptation_layer/data/siteinventory.py
+++ adaptation_layer/data/siteinventory.py
@@ -16,7 +16,8 @@ from typing import List, Dict
 from requests import Response, get, ConnectionError, Timeout, \
     TooManyRedirects, URLRequired, HTTPError
 
-from error_handler import ServerError, NfvoNotFound, NfvoCredentialsNotFound
+from error_handler import ServerError, NfvoNotFound, NfvoCredentialsNotFound, \
+    Unauthorized
 
 
 class SiteInventory:
@@ -28,7 +29,7 @@ class SiteInventory:
 
     @property
     def url(self):
-        return 'http://{0}:{1}'.format(self.host, self.port)
+        return 'http://{0}:{1}/'.format(self.host, self.port)
 
     @staticmethod
     def _exec_get(url=None, params=None, headers=None) -> Response:
@@ -37,27 +38,41 @@ class SiteInventory:
                        verify=False, stream=True)
             resp.raise_for_status()
         except (ConnectionError, Timeout, TooManyRedirects, URLRequired) as e:
-            raise ServerError("Problem contacting site inventory: " + str(e))
+            raise ServerError('Problem contacting site inventory: ' + str(e))
         return resp
 
     def _get_nfvo(self, nfvo_id) -> Dict:
         try:
-            json = self._exec_get(
-                self.url + "/nfvOrchestrators/" + nfvo_id).json()
+            nfvo = self._exec_get(
+                self.url + 'nfvOrchestrators/' + nfvo_id).json()
         except HTTPError as e:
             if e.response.status_code == 404:
                 raise NfvoNotFound(nfvo_id)
+            elif e.response.status_code == 401:
+                raise Unauthorized()
             else:
-                raise ServerError()
-        return json
+                raise
+        return nfvo
+
+    def _convert_nfvo(self, nfvo: Dict) -> Dict:
+        site = self._exec_get(nfvo['_links']['site']['href']).json()['name']
+        conv = {
+            'id': nfvo['id'],
+            'name': nfvo['name'],
+            'type': nfvo['type'],
+            'site': site
+        }
+        if nfvo['uri'] is not None:
+            conv['uri'] = nfvo['uri']
+        if nfvo['createdAt'] is not None:
+            conv['createdAt'] = nfvo['createdAt']
+        if nfvo['updatedAt'] is not None:
+            conv['updatedAt'] = nfvo['updatedAt']
+        return conv
 
     def get_nfvo_by_id(self, nfvo_id) -> Dict:
-        json = self._get_nfvo(nfvo_id)
-        return {
-            'id': json['id'],
-            'name': json['name'],
-            'type': json['type'],
-        }
+        nfvo = self._get_nfvo(nfvo_id)
+        return self._convert_nfvo(nfvo)
 
     def get_nfvo_cred(self, nfvo_id) -> Dict:
         nfvo = self._get_nfvo(nfvo_id)
@@ -70,12 +85,6 @@ class SiteInventory:
             return nfvo['credentials']
 
     def get_nfvo_list(self) -> List[Dict]:
-        json = self._exec_get(self.url + "/nfvOrchestrators").json()
-        nfvo_list = []
-        for nfvo in json['_embedded']['nfvOrchestrators']:
-            nfvo_list.append({
-                'id': nfvo['id'],
-                'name': nfvo['name'],
-                'type': nfvo['type'],
-            })
-        return nfvo_list
+        json = self._exec_get(self.url + 'nfvOrchestrators').json()
+        return [self._convert_nfvo(nfvo) for nfvo in
+                json['_embedded']['nfvOrchestrators']]
