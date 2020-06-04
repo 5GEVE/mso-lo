69d1029d488b7ce48b3c0009f6130b2eff3f44fc
FabioUbaldi <45456974+FabioUbaldi@users.noreply.github.com>
2020-06-04 11:36:27 +0200

    Update test_ever.py

--- adaptation_layer/tests/test_ever.py
+++ adaptation_layer/tests/test_ever.py
@@ -1,4 +1,4 @@
-#  Copyright 2019 Orange, Grzesik Michal, Panek Grzegorz, Chabiera Michal
+#  Copyright 2020 Ericsson TEI, Fabio Ubaldi
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
