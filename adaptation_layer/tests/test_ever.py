0e69c5c03f60fedebb09404df745ae3c86fce635
Matteo Pergolesi <matpergo@gmail.com>
2020-09-02 17:47:03 +0200

    Fix imports and remove body from terminate

--- adaptation_layer/tests/test_ever.py
+++ adaptation_layer/tests/test_ever.py
@@ -19,7 +19,7 @@ from jsonschema import validate
 from jsonschema.exceptions import ValidationError, SchemaError
 
 from adaptation_layer import create_app
-from .request_mock import mock_ns, mock_ns_terminate
+from .request_mock import mock_ns
 from .response_schemas import ns_lcm_op_occ_schema, \
     ns_list_schema, ns_schema, ns_lcm_op_occ_list_schema
 
@@ -100,8 +100,7 @@ class EverTestCase(unittest.TestCase):
 
     # Check status codes 202, 404, headers and payload for terminate_ns()
     def test_terminate_ns_202(self):
-        res = self.client().post('/rano/3/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=202',
-                                 json=mock_ns_terminate)
+        res = self.client().post('/rano/3/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=202')
         self.assertEqual(res.status_code, 202)
 
         self.assertIn('Location', res.headers)
@@ -109,8 +108,7 @@ class EverTestCase(unittest.TestCase):
         self.assertTrue(all([validate_url.scheme, validate_url.netloc, validate_url.path]))
 
     def test_terminate_ns_404(self):
-        res = self.client().post('/rano/3/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=404',
-                                 json=mock_ns_terminate)
+        res = self.client().post('/rano/3/ns_instances/49ccb6a2-5bcd-4f35-a2cf-7728c54e48b7/terminate?__code=404')
         self.assertEqual(res.status_code, 404)
 
     # Check status codes 204, 404, headers and payload for delete_ns()
